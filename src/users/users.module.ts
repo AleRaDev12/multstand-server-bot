2bb14952f8f7c63ee5ab02220203cf97fc842eae
jiexa <test@test.com>
2024-03-10 16:47:36 +0700

    wip: wizards (orders work)

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,24 @@ import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { AddUserWizard } from './wizards/add-user.wizard';
 import { BotUpdate } from '../bot.update';
+import { AddOrderWizard } from '../orders/add-order.wizard';
+import { OrdersService } from '../orders/orders.service';
+import { OrdersController } from '../orders/orders.controller';
+import { Order } from '../orders/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, AddUserWizard, BotUpdate],
-  controllers: [UsersController],
-  exports: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Order]),
+  ],
+  providers: [
+    BotUpdate,
+    UsersService,
+    AddUserWizard,
+    OrdersService,
+    AddOrderWizard,
+  ],
+  controllers: [UsersController, OrdersController],
+  exports: [UsersService, OrdersService],
 })
 export class UsersModule {}
