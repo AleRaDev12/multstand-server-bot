3bda6b8e1a1652d5c1bb807ea5f8487c3500df0d
Aleradev12 <aleradev12@gmail.com>
2024-03-23 18:27:06 +0700

    wip: wizard refactor

--- src/entities/stands/standAdd.wizard.ts
+++ src/entities/stands/standAdd.wizard.ts
@@ -11,6 +11,78 @@ import { Inject } from '@nestjs/common';
 import { CustomWizardContext } from '../../shared/interfaces';
 import { WIZARDS } from '../../shared/wizards'; // Новый импорт
 
+interface WizardStep {
+  message: string;
+  field: keyof Stand;
+  enum?: object;
+}
+
+const steps: WizardStep[] = [
+  { message: 'Модель:', field: 'model', enum: StandModel },
+  { message: 'Покраска:', field: 'painting', enum: PaintingType },
+  { message: 'Количество обычных стёкол:', field: 'glassesRegular' },
+];
+
+function generateMessage(step: WizardStep): string {
+  return step.enum ? `${step.message}\n${printEnum(step.enum)}` : step.message;
+}
+
+function WizardStepHandler(stepIndex: number) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    // const originalMethod = descriptor.value;
+
+    descriptor.value = async function (
+      ctx: CustomWizardContext,
+      ...args
+      // msg: { text: string },
+    ) {
+      const { message: msg } = ctx;
+
+      const step = steps[stepIndex];
+      console.log('*-* step', step);
+      console.log('*-* msg', msg);
+      console.log('*-* msg json', JSON.stringify(msg, null, 2));
+
+      if (step.enum) {
+        if (msg.text !== undefined) {
+          ctx.wizard.state.stand[step.field] = getValueByIndex(
+            step.enum,
+            +msg.text - 1,
+          );
+        } else {
+          // Обработка случая, когда msg.text равно undefined
+          // Например, отправка сообщения об ошибке и повторный запрос значения
+          return 'Некорректный ввод. Пожалуйста, введите значение еще раз.';
+        }
+      } else {
+        if (msg.text !== undefined) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          ctx.wizard.state.stand[step.field] = +msg.text;
+        } else {
+          // Обработка случая, когда msg.text равно undefined
+          return 'Некорректный ввод. Пожалуйста, введите значение еще раз.';
+        }
+      }
+
+      if (stepIndex === steps.length - 1) {
+        const stand = await this.usersService.create(ctx.wizard.state.stand);
+        await ctx.scene.leave();
+        return `Станок ${JSON.stringify(stand, null, 2)} добавлен`;
+      } else {
+        ctx.wizard.next();
+        return generateMessage(steps[stepIndex + 1]);
+      }
+    };
+
+    return descriptor;
+  };
+}
+
 @Wizard(WIZARDS.ADD_STAND_WIZARD_ID)
 export class StandAddWizard {
   constructor(
@@ -19,53 +91,24 @@ export class StandAddWizard {
   ) {}
 
   @WizardStep(1)
-  async s1(@Ctx() ctx: CustomWizardContext): Promise<string> {
+  async start(@Ctx() ctx: CustomWizardContext): Promise<string> {
     ctx.wizard.state.stand = new Stand();
     ctx.wizard.next();
-    return `Модель:\n${printEnum(StandModel)}`;
+    return generateMessage(steps[0]);
   }
 
   @On('text')
   @WizardStep(2)
-  async s2(
-    @Ctx() ctx: CustomWizardContext,
-    @Message() msg: { text: string },
-  ): Promise<string> {
-    ctx.wizard.state.stand.model = getValueByIndex(StandModel, +msg.text - 1);
-
-    ctx.wizard.next();
-
-    return `Покраска:\n${printEnum(PaintingType)}`;
-  }
+  @WizardStepHandler(0)
+  async step1() {}
 
   @On('text')
   @WizardStep(3)
-  async s3(
-    @Ctx() ctx: CustomWizardContext,
-    @Message() msg: { text: string },
-  ): Promise<string> {
-    ctx.wizard.state.stand.painting = getValueByIndex(
-      PaintingType,
-      +msg.text - 1,
-    );
-
-    ctx.wizard.next();
-
-    return `Количество обычных стёкол:`;
-  }
+  @WizardStepHandler(1)
+  async step2() {}
 
   @On('text')
   @WizardStep(4)
-  async last(
-    @Ctx() ctx: CustomWizardContext,
-    @Message() msg: { text: string },
-  ): Promise<string> {
-    ctx.wizard.state.stand.glassesRegular = +msg.text;
-
-    const stand = await this.usersService.create(ctx.wizard.state.stand);
-
-    await ctx.scene.leave();
-
-    return `Станок ${JSON.stringify(stand, null, 2)} добавлен`;
-  }
+  @WizardStepHandler(2)
+  async last() {}
 }
