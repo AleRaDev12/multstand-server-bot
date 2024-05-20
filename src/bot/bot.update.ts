c1b77e54167d7ec0e263d4d8fc046c0b9713602a
Aleradev12 <aleradev12@gmail.com>
2024-05-20 10:20:57 +0700

    feat: add persistent start button

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,11 +1,12 @@
 import { SCENES } from '../shared/wizards';
 import { Action, Ctx, Start, Update } from 'nestjs-telegraf';
-import { Scenes } from 'telegraf';
+import { Markup, Scenes } from 'telegraf';
 
 @Update()
 export class BotUpdate {
   @Start()
   async onStart(@Ctx() ctx: Scenes.SceneContext): Promise<void> {
+    await ctx.reply('Start', Markup.keyboard(['/start']).persistent().resize());
     await ctx.scene.enter(SCENES.ENTERING);
   }
 
