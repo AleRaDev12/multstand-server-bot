3f016e2f5d1057c4d6f807f0315e69666685490d
Aleradev12 <aleradev12@gmail.com>
2024-06-05 20:56:46 +0700

    feat: format orders list

--- src/entities/order/order.service.ts
+++ src/entities/order/order.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from './order.entity';
+import { addDays, format, differenceInDays } from 'date-fns';
 
 @Injectable()
 export class OrderService {
@@ -20,6 +21,27 @@ export class OrderService {
 
   async getList(): Promise<string> {
     const orders = await this.findAll();
-    return `${orders.map((order, i) => `\n${i + 1}. ${order.id} ${order.contractDate} ${order.client.firstName}`)}`;
+    return orders
+      .map((order, i) => this.formatOrder(order, i + 1))
+      .join('\n\n');
+  }
+
+  private formatOrder(order: Order, index: number): string {
+    const { client, contractDate, daysToComplete } = order;
+    const deliveryDeadline = addDays(contractDate, daysToComplete);
+
+    const daysUntilSend = differenceInDays(deliveryDeadline, new Date());
+    const daysUntilDelivery = '-';
+
+    const clientInfo = [
+      `${index}. ${client.firstName} ${client.lastName ?? ''}`,
+      client.organization ? `Организация: ${client.organization}` : null,
+      `Город: ${client.city}`,
+      `Дата заказа: ${format(contractDate, 'yyyy-MM-dd')}`,
+      `Крайний срок отправки: ${format(deliveryDeadline, 'yyyy-MM-dd')}, осталось ${daysUntilSend} дней`,
+      `Крайний срок доставки: ${daysUntilDelivery}, осталось ${daysUntilDelivery} дней`,
+    ].filter(Boolean);
+
+    return clientInfo.join('\n');
   }
 }
