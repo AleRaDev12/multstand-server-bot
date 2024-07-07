5291c7944232d0a5ceebe57998f40b2f6f3eeae0
Aleradev12 <aleradev12@gmail.com>
2024-07-07 17:20:31 +0700

    fix: invalid import

--- src/entities/parts/component/component-list.scene.ts
+++ src/entities/parts/component/component-list.scene.ts
@@ -2,10 +2,12 @@ import { Ctx, Scene, SceneEnter } from 'nestjs-telegraf';
 import { SCENES } from '../../../shared/scenes-wizards';
 import { Inject } from '@nestjs/common';
 import { Scenes } from 'telegraf';
-import { handleButtonPress } from '../../../shared/helpers';
+import {
+  formatWithListIndexes,
+  handleButtonPress,
+} from '../../../shared/helpers';
 import { ComponentService } from './component.service';
 import { SceneRoles } from '../../../bot/decorators/scene-roles.decorator';
-import { formatWithListIndexes } from '../../../../dist/src/shared/helpers';
 
 @Scene(SCENES.COMPONENT_LIST)
 @SceneRoles('manager')
