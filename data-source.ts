7cc4d51861d4df0b7c511af5d521946b4d1c3352
Aleradev12 <aleradev12@gmail.com>
2024-10-11 05:39:53 +0300

    fix: remove unused file

--- data-source.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from 'typeorm';
-
-export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'database.sqlite',
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/**/*.ts'],
-  synchronize: false,
-});
